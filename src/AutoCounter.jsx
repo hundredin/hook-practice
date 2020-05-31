1cd4a76dec5dcf1b8ba95f67d060cde8380dbb73
Seungbeom Baek <hundredin9@gmail.com>
2020-05-31 23:15:57 +0900

    6. deps 제거하기 - useReducer

--- src/AutoCounter.jsx
+++ src/AutoCounter.jsx
@@ -1,27 +1,50 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer, useEffect } from "react";
+
+const initialState = {
+  count: 0,
+  step: 1,
+};
+
+const reducer = (state, action) => {
+  const { count, step } = state;
+  if (action.type === "tick") {
+    return { count: count + step, step };
+  } else if (action.type === "step") {
+    return { count, step: action.step };
+  } else {
+    throw new Error();
+  }
+};
 
 const AutoCounter = () => {
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
 
   useEffect(() => {
     const id = setInterval(() => {
-      setCount((count) => count + step);
+      dispatch({ type: "tick" });
     }, 1000);
 
     // step 이 바뀐다고 인터벌이 초기화되지 않는 것을 원한다면?
+    // useReducer 를 이용해서 의존성배열(deps) 를 모두 제거했다.
+    // 결국 이 AutoCounter 컴포넌트는 한번만 실행되게 되며(=유지되며) dispatch 는 항상 같다.
     return () => {
       console.log("clear interval");
       clearInterval(id);
     };
-  }, [step]);
+  }, [dispatch]);
 
   return (
     <div>
       <h1>Counter</h1>
       <div className="count">{count}</div>
       step:{" "}
-      <input value={step} onChange={(e) => setStep(Number(e.target.value))} />
+      <input
+        value={step}
+        onChange={(e) => {
+          dispatch({ type: "step", step: Number(e.target.value) });
+        }}
+      />
     </div>
   );
 };
